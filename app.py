5473629fbd2c556432616211d7350589eeb12d9a
hardikkum444 <hardikkumawat444@gmail.com>
2024-04-20 14:01:55 +0530

    adding sqlite DB

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' 
+#using easy sqlite ///-> relative path ////-> absolute path
+
+db = SQLALCHEMY(app) #initialising the database
+
+#creating the databse model
+class Todo(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow) #for basic book-keeping
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
