b9f844ad265b68a3046dadc670faa682ad16778c
hardikkum444 <hardikkumawat444@gmail.com>
2024-04-20 22:20:20 +0530

    task_name error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -21,9 +21,20 @@ class Todo(db.Model):
 @app.route('/', methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        pass
+
+        task_content = request.form['content']
+        new_task = Todo(content="task_content")
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+
+        except:
+            return "Error"
     else:
-        return render_template('index.html')
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 with app.app_context():
     db.create_all()
