6ac1ae5e3396c122764cd0970b004e358659d50e
hardikkum444 <hardikkumawat444@gmail.com>
2024-04-21 10:31:47 +0530

    adding deletion functionality

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def index():
     if request.method == 'POST':
 
         task_content = request.form['content']
-        new_task = Todo(content="task_content")
+        new_task = Todo(content=task_content)
 
         try:
             db.session.add(new_task)
@@ -35,6 +35,18 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Error"
 
 with app.app_context():
     db.create_all()
